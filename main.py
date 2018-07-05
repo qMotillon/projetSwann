d0e630d5f75d6a7c5a349f1bc4aa9f3bcde54e9a
unknown <quentin.motillon@supinfo.com>
2018-07-06 00:56:06 +0200

    Login

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from users import Users
 
 user=Users()
+user.login();
 print(user.username)
 a=input("Ecris ce que tu veux")
--- users.py
+++ users.py
@@ -7,15 +7,15 @@ class Users:
 	passwordHash= "password"
 	mail = "test@test.com"
 	site = "Nantes"
-
-	def __init__(self):
+	c = database.conn.cursor()
+	def login(self):
 		self.nom = input("Quel est son nom ?\n")
 		self.prenom = input("Quel est son prénom ?\n")
 		self.username = self.prenom[0] + self.nom
 		self.passwordHash = input("Entre ton mdp\n")
 		self.passwordHash = md5(self.passwordHash.encode('utf-8')).hexdigest()
 		self.mail = input("Entre son mail lol \n")
-		self.site = int(input("Site : 1 : Paris ,2 : Nantes ,3 : Lyon ,4 : Strasbourg\n"))
+		self.site = int(input("Site 1: Paris, Site 2 : Nantes, Site 3 : Lyon, Site 4 : Strasbourg\n"))
 		if(self.site == 1):
 			self.site="Paris"
 		elif(self.site == 2):
@@ -33,5 +33,18 @@ class Users:
 		#c.execute("INSERT INTO users (id,nom,prenom,username,passwordHash,mail,site)")
 		database.conn.commit()
 
-		def login(self):
-			
+	def __init__(self):
+		print("____________________________________________\n")
+		username = input("Entrez votre pseudo : \n")
+		password = input("Entre votre mdp : \n")
+		password = md5(password.encode('utf-8')).hexdigest()
+		print(password+"\n")
+		c = database.conn.cursor()
+		c.execute("SELECT passwordHash FROM users WHERE username=:pseudo",{"pseudo" : username})
+		passwordDB = c.fetchone()
+		if(password == passwordDB[0]):
+			print("Connexion Réussi !!\n")
+			self.menu()
+		else:
+			print("U SUCKER\n")
+			self.__init__();
